592fb0a976dee7bff8ba7afe693453ecbfd7af2d
Reedip Adhikary <reedipadhikary@gmail.com>
2021-10-31 12:34:30 +0530

    Upload image to firebase

--- AdminApp/app/src/main/java/com/appone/jainadmin/UploadImage.java
+++ AdminApp/app/src/main/java/com/appone/jainadmin/UploadImage.java
@@ -1,9 +1,11 @@
 package com.appone.jainadmin;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -15,7 +17,19 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class UploadImage extends AppCompatActivity {
@@ -28,6 +42,11 @@ public class UploadImage extends AppCompatActivity {
     private String category;
     private final int REQ = 1;
     private Bitmap bitmap;
+    ProgressDialog pd;
+
+    private DatabaseReference reference;
+    private StorageReference storageReference;
+    String downloadUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +56,11 @@ public class UploadImage extends AppCompatActivity {
         imageCategory = findViewById(R.id.image_category);
         uploadImage = findViewById(R.id.uploadImageBtn);
         galleryImageView = findViewById(R.id.galleryImageView);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("gallery");
+        storageReference = FirebaseStorage.getInstance().getReference().child("gallery");
+
+        pd = new ProgressDialog(this);
 
         String[] items = new String[]{"Select Category", "Freshers", "Cultural Fest", "Other Events"};
         imageCategory.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items));
@@ -58,6 +82,72 @@ public class UploadImage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openGallery();
+            }
+        });
+
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bitmap == null){
+                    Toast.makeText(UploadImage.this, "Select an Image", Toast.LENGTH_SHORT).show();
+                }else if(category.equals("Select Category")){
+                    Toast.makeText(UploadImage.this, "Select a category", Toast.LENGTH_SHORT).show();
+                }else{
+                    pd.setMessage("Uploading");
+                    pd.show();
+                    uploadImage();
+                }
+            }
+        });
+    }
+
+    private void uploadImage() {
+        ByteArrayOutputStream baos =new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+        byte[] finalimg = baos.toByteArray();
+        final StorageReference filepath;
+        filepath = storageReference.child(finalimg+"jpg");
+        final UploadTask uploadTask =  filepath.putBytes(finalimg);
+        uploadTask.addOnCompleteListener(UploadImage.this, new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    downloadUrl = String.valueOf(uri);
+                                    uploadData();
+                                }
+                            });
+                        }
+                    });
+                }
+                else{
+                    pd.dismiss();
+                    Toast.makeText(UploadImage.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void uploadData() {
+        reference = reference.child(category);
+        final String uniqueKey = reference.push().getKey();
+
+        reference.child(uniqueKey).setValue(downloadUrl).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                pd.dismiss();
+                Toast.makeText(UploadImage.this, "Upload Successful", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(UploadImage.this, "Something went wrong", Toast.LENGTH_SHORT).show();
             }
         });
     }
