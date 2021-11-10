9a276fc7c9f0415b7c7e90eda2ae244f72cff2c2
Reedip Adhikary <reedipadhikary@gmail.com>
2021-11-10 07:03:29 +0530

    add teacher

--- AdminApp/app/src/main/java/com/appone/jainadmin/faculty/AddTeacher.java
+++ AdminApp/app/src/main/java/com/appone/jainadmin/faculty/AddTeacher.java
@@ -1,8 +1,10 @@
 package com.appone.jainadmin.faculty;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -15,10 +17,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.appone.jainadmin.NoticeData;
 import com.appone.jainadmin.R;
+import com.appone.jainadmin.UploadNotice;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class AddTeacher extends AppCompatActivity {
 
@@ -30,6 +47,11 @@ public class AddTeacher extends AppCompatActivity {
     private final int REQ = 1;
     private Bitmap bitmap = null;
     private String category;
+    private String name, email, post, downloadUrl = "";
+    private ProgressDialog pd;
+
+    private StorageReference storageReference;
+    private DatabaseReference reference, dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +64,11 @@ public class AddTeacher extends AppCompatActivity {
         addTeacherPost = findViewById(R.id.addTeacherPost);
         addTeacherCategory = findViewById(R.id.addTeacherCategory);
         addTeacherBtn = findViewById(R.id.addTeacherBtn);
+
+        pd = new ProgressDialog(this);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("teacher");
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         String[] items = new String[]{"Select Category", "Computer Science", "Mechanical", "Electrical"};
         addTeacherCategory.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items));
@@ -63,6 +90,94 @@ public class AddTeacher extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openGallery();
+            }
+        });
+
+        addTeacherBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkValidation();
+            }
+        });
+    }
+
+    private void checkValidation() {
+        name = addTeacherName.getText().toString();
+        email = addTeacherEmail.getText().toString();
+        post = addTeacherPost.getText().toString();
+
+        if(name.isEmpty()){
+            addTeacherName.setError("Add Name");
+            addTeacherName.requestFocus();
+        }else if(email.isEmpty()){
+            addTeacherEmail.setError("Add Email");
+            addTeacherEmail.requestFocus();
+        }else if(post.isEmpty()){
+            addTeacherPost.setError("Add Post");
+            addTeacherPost.requestFocus();
+        }else if(category.equals("Select Category")){
+            Toast.makeText(this, "Select a category", Toast.LENGTH_SHORT).show();
+        }else if(bitmap == null){
+            pd.setMessage("Adding");
+            pd.show();
+            insertData();
+        }else{
+            pd.setMessage("Adding");
+            pd.show();
+            uploadImage();
+        }
+    }
+
+    private void uploadImage() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+        byte[] finalimg = baos.toByteArray();
+        final StorageReference filepath;
+        filepath = storageReference.child("Teachers").child(finalimg+"jpg");
+        final UploadTask uploadTask =  filepath.putBytes(finalimg);
+        uploadTask.addOnCompleteListener(AddTeacher.this, new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    downloadUrl = String.valueOf(uri);
+                                    insertData();
+                                }
+                            });
+                        }
+                    });
+                }
+                else{
+                    pd.dismiss();
+                    Toast.makeText(AddTeacher.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void insertData() {
+        dbRef = reference.child(category);
+        final String uniqueKey = dbRef.push().getKey();
+
+        TeacherData teacherData = new TeacherData(name, email, post, downloadUrl, uniqueKey);
+        dbRef.child(uniqueKey).setValue(teacherData).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                pd.dismiss();
+                Toast.makeText(AddTeacher.this, "Teacher Added", Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(AddTeacher.this, "Something went wrong", Toast.LENGTH_SHORT).show();
             }
         });
     }
