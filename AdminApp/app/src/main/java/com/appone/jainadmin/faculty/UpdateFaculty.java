600b44387c3d57fd7c582ddbf65efa233040ea8e
Reedip Adhikary <reedipadhikary@gmail.com>
2021-12-30 14:18:36 +0530

    Teacher ui update

--- AdminApp/app/src/main/java/com/appone/jainadmin/faculty/UpdateFaculty.java
+++ AdminApp/app/src/main/java/com/appone/jainadmin/faculty/UpdateFaculty.java
@@ -1,22 +1,54 @@
 package com.appone.jainadmin.faculty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.appone.jainadmin.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UpdateFaculty extends AppCompatActivity {
 
     FloatingActionButton fab;
+    private RecyclerView csDepartment, mechanicalDepartment, electricalDepartment, aerospaceDepartment;
+    private LinearLayout csNoData, mechNoData, electricalNoData, aerospaceNoData;
+    private List<TeacherData> list1, list2, list3, list4;
+
+    private DatabaseReference reference, dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_faculty);
+
+        csDepartment = findViewById(R.id.csDepartment);
+        mechanicalDepartment = findViewById(R.id.mechanicalDepartment);
+        electricalDepartment = findViewById(R.id.electricalDepartment);
+        aerospaceDepartment = findViewById(R.id.aerospaceDepartment);
+
+        csNoData = findViewById(R.id.csNoData);
+        mechNoData = findViewById(R.id.mechNoData);
+        electricalNoData = findViewById(R.id.electricalNoData);
+        aerospaceNoData = findViewById(R.id.aerospaceNoData);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("teacher");
+        
+        csDepartment();
 
         fab = findViewById(R.id.fab);
 
@@ -24,6 +56,32 @@ public class UpdateFaculty extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(UpdateFaculty.this, AddTeacher.class));
+            }
+        });
+    }
+
+    private void csDepartment() {
+        dbRef = reference.child("Computer Science");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list1 = new ArrayList<>();
+                if (!dataSnapshot.exists()){
+                    csNoData.setVisibility(View.VISIBLE);
+                    csDepartment.setVisibility(View.GONE);
+                }else{
+                    for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                        TeacherData data = snapshot.getValue(TeacherData.class);
+                        list1.add(data);
+                    }
+                    csDepartment.setHasFixedSize(true);
+                    csDepartment.setLayoutManager(new LinearLayoutManager(UpdateFaculty.this));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(UpdateFaculty.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
